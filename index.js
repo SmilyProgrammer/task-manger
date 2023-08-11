e9a51602db7154a796864b054db7fd2fcdf3ff7a
SmilyProgrammer <hassanmurtaza0571@gmail.com>
2023-08-11 18:06:28 +0500

    After Refactoring the code Project is Completed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
 app.use(express.static("./public"));
@@ -16,19 +17,14 @@ app.get("/hello", (req, res) => {
 
 app.use("/api/v1/tasks", tasks);
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-// app.get('/api/v1/tasks')         - get all the tasks
-// app.post('/api/v1/tasks')        - create a new task
-// app.get('/api/v1/tasks:id')      - get single task
-// app.patch('/api/v1/tasks/:id')   - update task
-// app.delete('/api/v1/tasks/:id')  - delete
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, console.log(`Server is Running on Port ${port}...`));
+    app.listen(port, console.log(`Server is listening on Port ${port}...`));
   } catch (error) {
     console.log(error);
   }
